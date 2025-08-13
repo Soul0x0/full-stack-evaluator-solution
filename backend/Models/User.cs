f7714440b8f6c27795b564b9e98ba83d3d31b784
Miguel <miguel.magno03@gmail.com>
2025-08-13 20:11:59 +0800

    Fix for infinite recursion
    Due both User and Task calling each other causing infinite loop added fix to ignore if not needed

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,12 +1,12 @@
+using System.Text.Json.Serialization;
+using TaskManager.Models;
 
-
-namespace TaskManager.Models
-{
-    public class User
+public class User
 {
     public int Id { get; set; }
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+
+    [JsonIgnore]
     public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
-}
 }
